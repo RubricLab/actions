2bae6fd01dfe854928655b44b9a8fb411703c1fd
DexterStorey <dexter@dexterstorey.com>
2024-10-02 21:24:26 -0400

    gitignore, biome, vscode

--- lib/types.ts
+++ lib/types.ts
@@ -1,21 +1,19 @@
-import type { z } from "zod";
+import type { z } from 'zod'
 
-export type ActionSchema = z.ZodTypeAny;
+export type ActionSchema = z.ZodTypeAny
 
 export interface ActionContext {
-	userId: string;
+	userId: string
 }
 
 interface BaseAction<T extends ActionSchema, R> {
-	schema: T;
-	handler: (params: unknown, context: ActionContext) => Promise<R>;
+	schema: T
+	handler: (params: unknown, context: ActionContext) => Promise<R>
 }
 
 export interface Action<T extends ActionSchema, R> extends BaseAction<T, R> {
-	handler: (params: z.infer<T>, context: ActionContext) => Promise<R>;
+	handler: (params: z.infer<T>, context: ActionContext) => Promise<R>
 }
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export type HandlerReturnType<H> = H extends (...args: any) => Promise<infer R>
-	? R
-	: never;
+export type HandlerReturnType<H> = H extends (...args: any) => Promise<infer R> ? R : never
