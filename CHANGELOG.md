238e4ece750adc89f40ead848719d9953f31d1b3
DexterStorey <dexter@dexterstorey.com>
2024-10-02 21:08:22 -0400

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+- [2024-10-02] [init](https://github.com/RubricLab/actions/commit/8cac0caf408b8418820f1c1ab03ff4f3a0fd0f3a)
+- [2024-10-02] [init](https://github.com/RubricLab/actions/commit/7d051dfcb6d43ae9daa9027d9f787ce0a259bed3)
+- [2024-10-02] [init](https://github.com/RubricLab/actions/commit/a65e3ab1946b5ef01ff0ea3d8443720b6ddf079a)
+# Changelog
+
---
+++ lib/utils.ts
@@ -0,0 +1,77 @@
+import { z } from 'zod'
+import type { Action, ActionContext, HandlerReturnType } from './types'
+
+export function createAction<T extends z.ZodTypeAny, R>(config: {
+	schema: T
+	handler: (params: z.infer<T>, context: ActionContext) => Promise<R>
+}): Action<T, R> {
+	return config
+}
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function createActionsSchemas<AP extends Record<string, Action<any, any>>>(actions: AP) {
+	type ActionName = keyof AP
+	return {
+		actionsSchemas: z.discriminatedUnion(
+			'action',
+			Object.entries(actions).map(([actionName, actionConfig]) =>
+				z.object({
+					action: z.literal(actionName as ActionName),
+					params: actionConfig.schema
+				})
+			) as [
+				z.ZodObject<{
+					action: z.ZodLiteral<ActionName>
+					params: (typeof actions)[ActionName]['schema']
+				}>,
+				...z.ZodObject<{
+					action: z.ZodLiteral<ActionName>
+					params: (typeof actions)[ActionName]['schema']
+				}>[]
+			]
+		)
+	}
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function createActionsActions<AP extends Record<string, Action<any, any>>>(actions: AP) {
+	type ActionName = keyof AP
+
+	const actionSchemas = z.discriminatedUnion(
+		'action',
+		Object.entries(actions).map(([actionName, actionConfig]) =>
+			z.object({
+				action: z.literal(actionName as ActionName),
+				params: actionConfig.schema
+			})
+		) as [
+			z.ZodObject<{
+				action: z.ZodLiteral<ActionName>
+				params: (typeof actions)[ActionName]['schema']
+			}>,
+			...z.ZodObject<{
+				action: z.ZodLiteral<ActionName>
+				params: (typeof actions)[ActionName]['schema']
+			}>[]
+		]
+	)
+
+	return {
+		actionSchemas,
+		async executeAction<A extends keyof AP>({
+			action,
+			params,
+			context
+		}: {
+			action: A
+			params: z.infer<AP[A]['schema']>
+			context: ActionContext
+		}): Promise<HandlerReturnType<AP[A]['handler']>> {
+			const actionConfig = actions[action]
+			if (!actionConfig) {
+				throw new Error(`Action not found: ${String(action)}`)
+			}
+			const validatedParams = actionConfig.schema.parse(params)
+			return actionConfig.handler(validatedParams, context)
+		}
+	}
+}
